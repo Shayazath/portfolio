12ea85d7a92154fa4ede60b03ab4e4d5cddb7894
shayaz <shayazath@i2global.io>
2026-03-31 12:05:04 +0530

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "../styles/index.css";
 export const metadata: Metadata = {
   title: "Know shayaz",
   description: "Full Stack Developer & Designer Portfolio",
+   icons: {
+    icon: "/icon.jpg", // or /favicon.ico
+  },
 };
 
 export default function RootLayout({
